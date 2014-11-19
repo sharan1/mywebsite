a2897f1c0da59407c03c4a9fffdec4f980ceeb64
Sharan Girdhani <sharan.girdhani@gmail.com>
2014-11-19 09:26:59 +0530

    Email sent to the user itself

--- saveDetails.php
+++ saveDetails.php
@@ -2,7 +2,6 @@
 require_once('connect.php') ;
 ini_set('display_errors',1);
 
-
 $name = $_POST['name'];
 $email = $_POST['email'] ;
 $mobile= $_POST['mobile'] ;
@@ -16,10 +15,19 @@ mysql_query($query)  or die( "Query Failed");
 mysql_close();
 
 $to = 'sharan.girdhani@gmail.com' ;
-$subject = ' Contact - Name: '.$name. ', Mobile No : '.$mobile;
+$subject = ' Contact - Name: '.$name. ', Mobile No : '.$mobile. ', Email : '.$email;
 $message = $reason ;
 $headers = 'From: MyWebsite <admin@sharangirdhani.com>';
 $status = mail($to, $subject, $message, $headers) ;
+
+$r_to = $email;
+$r_subject = 'Contact : sharangirdhani.com';
+$r_message = 'Thank You '.$name.' for contacting me. I will get back to you ASAP if your credentials are correct. :P';
+$r_headers = 'From: Sharan Girdhani <admin@sharangirdhani.com>';
+$recipient_status = mail($r_to, $r_subject, $r_message, $r_headers) ;
+
+
+
 
 header('Location: temp.html');  
 ?>
