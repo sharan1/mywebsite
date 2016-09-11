e67c83444d48930ddee8f30887be4e40ec37bd13
Sharan Girdhani <sharangirdhani@Sharans-MacBook-Pro.local>
2016-09-10 21:13:39 -0400

    Update profile basic

--- index.php
+++ index.php
@@ -7,102 +7,16 @@
 			</div>
 			<div class="col-md-7">
 				<br /><br /><br />
-				<p id="nameDetail"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Sharan Girdhani<br /><a href="#">Web Developer, CouponDunia</a> <br />&nbsp;&nbsp;<a href="#">sharan.girdhani@gmail.com</a></p>
+				<p id="nameDetail"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Sharan Girdhani<br /><a href="#">Graduate Student, UNC Charlotte</a> <br />&nbsp;&nbsp;<a href="#">sharan.girdhani@gmail.com</a></p>
 			</div>
 		</div>
 		<font face="Comic sans MS" size="4px">
   		<div class="row">
   			<p><i style="color:black">
   				<br /><br /><br />Hello,<br />
-  				I am Sharan, Software Engineer, working at <a href="http://www.coupondunia.in/" target="_blank">CouponDunia Media Pvt. Ltd, Mumbai</a>, B-Tech Graduate from <a href="http://www.iiit.ac.in/" target="_blank" >International Institute of Information Technology, Hyderabad</a>(IIIT-H), India.
+  				I am Sharan, a Graduate Student, at <a href="http://www.uncc.edu/" target="_blank">University of North Carolina, Charlotte</a>, B-Tech Under-Graduate from <a href="http://www.iiit.ac.in/" target="_blank" >International Institute of Information Technology, Hyderabad(IIIT-H), India.</a>
   			</i></p>
   		</div>
     </font>
   </div>
-
-	<!-- Carousel
-	============================= -->
-
-<!--	<div id="myCarousel" class="carousel slide">
-		<ol class="carousel-indicators" >
-			<li data-target ="#myCarousel" data-slide-to="0" class="active"></li>
-			<li data-target ="#myCarousel" data-slide-to="1"></li>
-			<li data-target ="#myCarousel" data-slide-to="2"></li>
-		</ol>
-
-		<div class="carousel-inner"  >
-			<div class="item active">
-				<img src="img/image6.jpg" style="height:600px;width:100%"/>
-				<div class="container">
-					<div class="carousel-caption">
-						<h1>My Heading text</h1>
-						<p>This tag will contain the text which we want to appear on our slide</p>
-						<p><a class="btn btn-large btn-primary">Sign-Up</a></p>
-					</div>
-				</div>
-			</div>
-
-			<div class="item">
-				<img src="img/image5.jpeg" style="height:600px;width:100%"/>
-				<div class="container">
-					<div class="carousel-caption">
-						<h1>My Heading text</h1>
-						<p>This tag will contain the text which we want to appear on our slide</p>
-						<p><a class="btn btn-large btn-primary">Sign-Up</a></p>
-					</div>
-				</div>
-			</div>
-
-			<div class="item">
-				<img src="img/image1.jpg" style="height:600px;width:100%"/>
-				<div class="container">
-					<div class="carousel-caption">
-						<h1>My Heading text</h1>
-						<p>This tag will contain the text which we want to appear on our slide</p>
-						<p><a class="btn btn-large btn-primary">Sign-Up</a></p>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<a class="left carousel-control" href="#myCarousel" data-slide="prev">
-			<span class="glyphicon glyphicon-chevron-left"></span>
-		</a>
-		<a class="right carousel-control" href="#myCarousel" data-slide="next">
-			<span class="glyphicon glyphicon-chevron-right"></span>
-		</a>
-	</div> -->
-
-	<!-- Grid
-	============================-->
-<!--	<br><br><br><br><br>
-	<div class="container">
-		<div class="row">
-			<div class="col-md-4">
-				<h2>First heading</h2>
-				<p>Some text will go here.Some text will go here.Some text will go here.Some text will go here.</p>
-			</div>
-			<div class="col-md-4">
-				<h2>First heading</h2>
-				<p>Some text will go here.Some text will go here.Some text will go here.Some text will go here.</p>
-			</div>
-			<div class="col-md-4">
-				<h2>First heading</h2>
-				<p>Some text will go here.Some text will go here.Some text will go here.Some text will go here.</p>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-md-6">
-				<h1>Second heading</h1>
-				<p>Some text will go here.Some text will go here.Some text will go here.Some text will go here.</p>
-			</div>
-			<div class="col-md-6">
-				<h1>Second heading</h1>
-				<p>Some text will go here.Some text will go here.Some text will go here.Some text will go here.</p>
-			</div>
-		</div>
-	</div>
--->
-
-
-	<?php include 'footer.php'; ?>
+<?php include 'footer.php'; ?>
