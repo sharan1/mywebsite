bd5317903e7c8ad02e544c200b4d30f9b9c31906
Sharan Girdhani <sgirdhan@uncc.edu>
2017-01-15 23:15:34 -0500

    Minor changes

--- internships.php
+++ internships.php
@@ -3,18 +3,18 @@
 	<div class="container" id="intern">
 	<font size="3px">
 	<hr/>
-		<h2 id="isb"><u><a href="http://www.isb.edu" target="_blank">Indian School of Business</a></u>:</h2>	
+		<h3 id="isb"><u><a href="http://www.isb.edu" target="_blank">Indian School of Business</a></u>:</h3>	
 		<p><i> 05/03/2013-07/02/2013, Internship at Indian School of Business(ISB),Hyderabad under Prof. Kannan Srikanth in Faculty & Research Department. As an intern I have worked on a project which involved the use of tools like MySQL, C programming and Stata.</i></p>
 
 		<img src="img/isb.jpg" class="img-rounded imglogo" width="400px" height="200px">
 		<br /><br />
 		
-		<h2 id="color"><u>ColorSplash</u>:</h2>	
+		<h3 id="color"><u>ColorSplash</u>:</h3>	
 		<p> <i>04/14/2012-06/30/2012, Business development trainee at Color Splash, a venture of Evonix Technologies, Pune which is mainly a T-Shirt design company.</i></p>
 		<img src="img/colorsplash.png" class="img-rounded imglogo" width="300px" height="200px">
 		<br /><br />
 		
-		<h2 id="trip"><u>TripNaksha</u>:</h2>	
+		<h3 id="trip"><u>TripNaksha</u>:</h3>	
 		<p> <i>06/01/2011-07/30/2011, Summer intern at a start-up company called TripNaksha (Hyderabad) which is an adventure travel company. My work here, as an Intern was to design a Facebook application for them.</i></p>
 		<img src="img/tripnaksha.jpg" class="img-rounded imglogo" width="200px" height="150px">
 		<br /><br />
